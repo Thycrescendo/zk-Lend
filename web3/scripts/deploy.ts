86f9342e957e44d65636e1d3d8d010bebf19fdd8
thycrescendo <dmicholn@gmail.com>
2024-08-19 16:45:01 +0100

    optimized the smart contract

--- web3/scripts/deploy.ts
+++ web3/scripts/deploy.ts
@@ -1,46 +1,27 @@
-const { ethers } = require('ethers');
-const { Provider, Wallet, ContractFactory } = require("zksync-ethers");
-require('dotenv').config();
+import { ethers } from 'hardhat';
+import console from 'console';
 
-// Import the compiled ABI and bytecode of the BattleGame contract
-const BattleGame = require('../artifacts-zk/contracts/BattleGame.sol/BattleGame.json');
+const _metadataUri = 'https://gateway.pinata.cloud/ipfs/https://gateway.pinata.cloud/ipfs/QmX2ubhtBPtYw75Wrpv6HLb1fhbJqxrnbhDo1RViW3oVoi';
 
-// Replace with your zkSync RPC URL and private key
-const ZKSYNC_RPC_URL = 'https://mainnet.era.zksync.io';
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+async function deploy(name: string, ...params: [string]) {
+  const contractFactory = await ethers.getContractFactory(name);
 
-if (!PRIVATE_KEY) {
-    console.error("Please set your PRIVATE_KEY in the .env file");
-    process.exit(1);
+  return await contractFactory.deploy(...params).then((f) => f.deployed());
 }
 
-async function deploy() {
-    // Connect to zkSync provider
-    const provider = new ethers.providers.JsonRpcProvider(ZKSYNC_RPC_URL);
-    
-    // Connect wallet using private key
-    const wallet = new Wallet(PRIVATE_KEY, provider);
+async function main() {
+  const [admin] = await ethers.getSigners();
+  
+  console.log(`Deploying a smart contract...`);
 
-    // Log wallet address
-    console.log('Deploying from wallet address:', wallet.address);
+  const AVAXGods = (await deploy('AVAXGods', _metadataUri)).connect(admin);
 
-    // Instantiate the ContractFactory for BattleGame
-    const BattleGameFactory = new ContractFactory(BattleGame.abi, BattleGame.bytecode, wallet);
-
-    // Example argument for the constructor
-    const initialCount = 5;
-
-    // Deploy the contract with the constructor arguments
-    const battleGame = await BattleGameFactory.deploy(initialCount);
-    await battleGame.deployed();
-
-    // Log deployed contract address
-    console.log('BattleGame deployed to:', battleGame.address);
+  console.log({ AVAXGods: AVAXGods.address });
 }
 
-deploy()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error('Error deploying BattleGame contract:', error);
-        process.exit(1);
-    });
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  });
