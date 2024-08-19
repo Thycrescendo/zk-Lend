416f70fa3e86c5fc5a7882a077cff6d8171d0bc0
thycrescendo <dmicholn@gmail.com>
2024-08-19 16:28:40 +0100

    higher order component utilization

---
+++ client/src/components/index.js
@@ -0,0 +1,23 @@
+import PageHOC from "./PageHOC";
+import CustomInput from "./CustomInput";
+import CustomButton from "./CustomButton";
+import Alert from "./Alert";
+import GameLoad from "./GameLoad";
+import PlayerInfo from "./PlayerInfo";
+import ActionButton from "./ActionButton";
+import Card from "./Card";
+import GameInfo from "./GameInfo";
+import OnBoardModal from './OnboardModal'
+
+export {
+    PageHOC,
+    CustomInput,
+    CustomButton,
+    Alert,
+    GameLoad,
+    PlayerInfo,
+    ActionButton,
+    Card,
+    GameInfo,
+    OnBoardModal
+}
