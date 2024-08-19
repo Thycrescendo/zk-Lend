34f6e6c708f13842bac77a1c4c3265352999a707
thycrescendo <dmicholn@gmail.com>
2024-08-19 16:42:41 +0100

    reconfigured test

--- web3/hardhat.config.ts
+++ web3/hardhat.config.ts
@@ -1,55 +1,39 @@
-import { HardhatUserConfig } from "hardhat/config";
-
-import "@matterlabs/hardhat-zksync";
-import * as dotenv from "dotenv";
+import dotenv from 'dotenv';
+import '@nomiclabs/hardhat-ethers';
 
 dotenv.config();
 
-const config: HardhatUserConfig = {
-  defaultNetwork: "zkSyncSepoliaTestnet",
-  networks: {
-    zkSyncSepoliaTestnet: {
-      url: "https://sepolia.era.zksync.dev",
-      ethNetwork: "sepolia",
-      zksync: true,
-      verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
-    },
-    zkSyncMainnet: {
-      url: "https://mainnet.era.zksync.io",
-      ethNetwork: "mainnet",
-      zksync: true,
-      verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
-    },
-    zkSyncGoerliTestnet: { // deprecated network
-      url: "https://testnet.era.zksync.dev",
-      ethNetwork: "goerli",
-      zksync: true,
-      verifyURL: "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
-    },
-    dockerizedNode: {
-      url: "http://localhost:3050",
-      ethNetwork: "http://localhost:8545",
-      zksync: true,
-    },
-    inMemoryNode: {
-      url: "http://127.0.0.1:8011",
-      ethNetwork: "localhost", // in-memory node doesn't support eth node; removing this line will cause an error
-      zksync: true,
-    },
-    hardhat: {
-      zksync: true,
-    },
-  },
-  zksolc: {
-    version: "latest",
-    settings: {
-      // find all available options in the official documentation
-      // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-solc.html#configuration
-    },
-  },
-  solidity: {
-    version: "0.8.17",
-  },
-};
+//* Notes for deploying the smart contract on your own subnet
+//* More info on subnets: https://docs.avax.network/subnets
+//* Why deploy on a subnet: https://docs.avax.network/subnets/when-to-use-subnet-vs-c-chain
+//* How to deploy on a subnet: https://docs.avax.network/subnets/create-a-local-subnet
+//* Transactions on the C-Chain might take 2-10 seconds -> the ones on the subnet will be much faster
+//* On C-Chain we're relaying on the Avax token to confirm transactions -> on the subnet we can create our own token
+//* You are in complete control over the network and it's inner workings
 
-export default config;
+export default {
+  solidity: {
+    version: '0.8.16',
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 100,
+      },
+    },
+  },
+  networks: {
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    // subnet: {
+    //   url: process.env.NODE_URL,
+    //   chainId: Number(process.env.CHAIN_ID),
+    //   gasPrice: 'auto',
+    //   accounts: [process.env.PRIVATE_KEY],
+    // },
+  },
+}
